b45ec95ef12983ce61f374855086d533df672b29
henry oginga <ogingahenry@gmail.com>
2018-11-16 14:18:21 +0300

    fix migration

--- db/migrate/20181113120832_create_cars.rb
+++ db/migrate/20181113120832_create_cars.rb
@@ -3,7 +3,7 @@ class CreateCars < ActiveRecord::Migration[5.2]
     create_table :cars do |t|
       t.string :make
       t.string :model
-      t.intege :year
+      t.integer :year
 
       t.timestamps
     end
